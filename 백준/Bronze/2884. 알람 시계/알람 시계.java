04303db1b95347da991f30e68f2bdb952a9abe58
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-06-30 20:51:41 +0900

    [Bronze III] Title: 알람 시계, Time: 232 ms, Memory: 18524 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if (B >= 45 && B <= 59) {
+            System.out.println(A + " " + (B-45));
+        } else if (A == 0) {
+            System.out.println(23 + " " + (B+15));
+        } else {
+            System.out.println((A-1) + " " + (B+15));
+        }
+    }
+}
