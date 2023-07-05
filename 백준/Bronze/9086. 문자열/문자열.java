ce2390ae7878a7ad75478200d10b5a07c6223626
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-07-05 12:45:57 +0900

    [Bronze V] Title: 문자열, Time: 140 ms, Memory: 14612 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            String s = br.readLine();
+            bw.write(s.charAt(0));
+            bw.write(s.charAt(s.length()-1) + "\n");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
