6dde049a3fed99393efd956b95af3581c44bb11e
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-07-05 10:55:18 +0900

    [Bronze I] Title: 평균, Time: 160 ms, Memory: 16060 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        double[] arr = new double[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        double max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (arr[i] / max * 100);
+        }
+
+        double count = 0;
+        for (int i = 0; i < arr.length; i++) {
+             count += arr[i];
+        }
+
+        bw.write((count/n) + "\n");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
