e44535213256378abf104257ba3e68df82532014
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-07-05 19:24:31 +0900

    [Bronze V] Title: 아스키 코드, Time: 148 ms, Memory: 15884 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+        int n = s.charAt(0);
+        bw.write(n + "\n");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
