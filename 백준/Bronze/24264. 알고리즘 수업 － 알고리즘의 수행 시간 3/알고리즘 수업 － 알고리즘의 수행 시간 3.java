ffbc393c457697a3cba2f582f0e90e730d52d9af
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-07-15 02:18:45 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 140 ms, Memory: 15756 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Long n = Long.parseLong(br.readLine());
+
+        bw.write(n*n + "\n");
+        bw.write(2 + "\n");
+
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
