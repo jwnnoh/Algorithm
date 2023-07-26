0e0b81f82e2c5634ef8e2b0a9fa0a2ca4a41770a
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-07-26 21:18:07 +0900

    [Silver V] Title: 영화감독 숌, Time: 332 ms, Memory: 86880 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int num = 666;
+        int count = 1;
+
+        while (count != n) {
+            num++;
+            if(String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        bw.write(num + "\n");
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
