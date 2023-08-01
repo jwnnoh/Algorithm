29d655a8880c1e17e8d47d20850adfb8289de595
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-08-01 23:16:59 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1788 ms, Memory: 318940 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -9,28 +9,20 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
-        int[] count = new int[10001];
-        int[] result = new int[n];
+        int[] arr = new int[10001];
 
         // Counting Sort
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[Integer.parseInt(st.nextToken())]++;
         }
-        for (int i : arr) {
-            count[i]++;
-        }
-        for (int i = 1; i < count.length; i++) {
-            count[i] += count[i - 1];
-        }
-        for (int i = arr.length - 1; i >= 0; i--) {
-            int tmp = arr[i];
-            count[tmp]--;
-            result[count[tmp]] = tmp;
-        }
-        for (int i : result) {
-            sb.append(i).append("\n");
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                for (int j = 0; j < arr[i]; j++) {
+                    sb.append(i).append("\n");
+                }
+            }
         }
         System.out.println(sb);
     }
