398570c7effa75d2f581706f7261ee2d4b8b251f
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-08-09 00:46:04 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("|\\_/|").append("\n");
+        sb.append("|q p|   /}").append("\n");
+        sb.append("( 0 )\"\"\"\\").append("\n");
+        sb.append("|\"^\"`    |").append("\n");
+        sb.append("||_/=\\\\__|");
+        System.out.println(sb);
+    }
+}
