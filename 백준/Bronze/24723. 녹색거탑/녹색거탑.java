a2cf8646cb3f44f8d9cc4f414bf36d06a35aaa63
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-08-10 00:13:34 +0900

    [Bronze IV] Title: 녹색거탑, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        System.out.println((int)Math.pow(2, Integer.parseInt(br.readLine())));
+    }
+}
