f23fc2187563f2e9ff677ef590f904373eba0b02
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-08-11 19:32:17 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 128 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -3,16 +3,19 @@ import java.util.*;
 
 
 public class Main {
+    public static long factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        else {
+            return n*factorial(n-1);
+        }
+
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-
-        long ans = 1;
-        while (n > 0) {
-            ans *= n;
-            n--;
-        }
-        System.out.println(ans);
+        System.out.println(factorial(n));
     }
 }
