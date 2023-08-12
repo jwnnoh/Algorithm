f52e7829ebaf511750894b77e0a835a3ca0a3d35
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-08-12 20:01:21 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 128 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static long fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(fibonacci(n));
+    }
+}
