018549568d4a9c6c5ed5c017525eee4e0e4cb506
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-08-21 21:28:52 +0900

    [Silver III] Title: N과 M (1), Time: 240 ms, Memory: 22820 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+
+    static int[] arr;
+    static boolean[] check;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        check = new boolean[n];
+
+        dfs(n, m, 0);
+
+        System.out.println(sb);
+    }
+
+    private static void dfs(int n, int m, int depth) {
+
+        if (depth == m) {
+            for (int i :
+                    arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            if (!check[i]) {
+                check[i] = true;
+
+                arr[depth] = i + 1;
+                dfs(n, m, depth + 1);
+
+                check[i] = false;
+            }
+        }
+    }
+}
