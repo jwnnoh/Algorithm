8cb3b3ea8c7d688733c67f96b16ff947172fa614
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-09-20 16:08:40 +0900

    [level 1] Title: 예산, Time: 0.41 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum = 0;
+        
+        Arrays.sort(d);
+        for(int i : d) {
+            if (budget - i < 0) {
+                break;
+            }
+            budget -= i;
+            answer++;
+        }
+        return answer;
+    }
+}
