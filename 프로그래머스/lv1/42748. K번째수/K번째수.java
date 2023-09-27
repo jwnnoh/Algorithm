15398dc84bcfd793436ebdfaf4c85b7463adc947
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-09-27 12:40:16 +0900

    [level 1] Title: K번째수, Time: 0.32 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,25 +1,14 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        
-        int[] sub;
-        int start = 0, end = 0;
-        for (int i = 0; i < commands.length; i++) {
-            end = commands[i][1] - 1;
-            start = commands[i][0] - 1; 
-            sub = new int[end - start + 1]; 
-            
-            int count = 0;
-            for (int j = start; j <= end; j++) {
-                sub[count] = array[j];
-                count++;
-            }
-            
-            Arrays.sort(sub);
-            answer[i] = sub[commands[i][2] - 1];
-        }
-        return answer;
+  public int[] solution(int[] array, int[][] commands) {
+      int[] answer = new int[commands.length];
+
+      for (int i = 0; i < commands.length; i++) {
+        int[] sub = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+        Arrays.sort(sub);
+        answer[i] = sub[commands[i][2] - 1];
     }
+    return answer;
+  }
 }
