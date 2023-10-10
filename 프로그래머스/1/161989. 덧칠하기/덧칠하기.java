91e3995d1754c4dbe907fc74e95935e65863c4ed
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-10-10 09:42:45 +0900

    [level 1] Title: 덧칠하기, Time: 0.98 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static int solution(int n, int m, int[] section) {
+        int answer = 1;
+        int start = section[0];
+        int end = start + m - 1;
+
+        for (int i : section) {
+            if (i >= start && i <= end) {
+                continue;
+            }
+            else {
+                start = i;
+                end = start + m - 1;
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
