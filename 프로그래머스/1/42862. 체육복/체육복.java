4e2b173dc6ebf7544ccae42f0c6c84564ae0cd27
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-10-18 10:04:50 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        int[] pe = new int[n+2];
+        Arrays.fill(pe, 1);
+        pe[0] = pe[n+1] = 0;
+        
+        for (int r : reserve) {
+            pe[r]++;
+        }
+        for (int l : lost) {
+            pe[l]--;
+        }
+        
+        for (int i = 1; i < n+1; i++) {
+            if (pe[i] == 0) {
+                if (pe[i-1] == 2) {
+                    pe[i-1]--;
+                    pe[i]++;
+                    continue;
+                }
+                if (pe[i+1] == 2) {
+                    pe[i+1]--;
+                    pe[i]++;
+                    continue;
+                }
+            }
+            
+        }
+        
+        for (int i : pe) {
+            if (i >= 1) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
