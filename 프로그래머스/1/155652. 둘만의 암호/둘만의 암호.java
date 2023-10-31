f61b6b1e79cf8029d2c5cd5232ac3962b3b0130c
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-10-31 10:18:29 +0900

    [level 1] Title: 둘만의 암호, Time: 0.85 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s, String skip, int index) {
+        StringBuilder answer = new StringBuilder();
+        
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < skip.length(); i++) {
+            list.add(skip.charAt(i) + 0);
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            int count = 0;
+            int tmp = s.charAt(i);
+            
+            while (count < index) { //97~122
+                tmp++;
+                
+                if (tmp > 122) {
+                    tmp -= 26;
+                }
+                if (list.contains(tmp)) {
+                    continue;
+                }
+                count++;
+            }
+            answer.append((char) tmp);
+        }
+        return answer.toString();
+    }
+}
