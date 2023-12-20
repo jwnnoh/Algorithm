5326f57c6fec3760e0b68112e8a08546911af8e8
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-12-20 23:47:11 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.21 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+ 
+public class Solution {
+        public int[] solution(int n, String[] words) {
+            int[] answer = new int[2];
+            List<String> list = new ArrayList<>();
+ 
+            for(int i = 0; i < words.length; i++){
+                if(list.contains(words[i])) {
+                    answer[0] = (i % n) + 1;
+                    answer[1] = (i / n) + 1;
+                    break;
+                }
+                list.add(words[i]);
+                if(i > 0 && words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0)) {
+                    answer[0] = (i % n) + 1;
+                    answer[1] = (i / n) + 1;
+                    break;
+                }
+            }
+ 
+            return answer;
+        }
+}
