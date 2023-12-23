eb889661324eb30330fe0aa4173fb2fc9efcb7e8
Jeongwan Noh <129377887+jwnnoh@users.noreply.github.com>
2023-12-23 14:56:50 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.10 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(int n) {
+        int[] arr = new int[2001];
+        arr[1] = 1; // 1
+        arr[2] = 2; // 1,1 | 2
+        
+        for (int i = 3; i < arr.length; i++) {
+            arr[i] = arr[i-2] + arr[i-1] % 1234567;
+        }
+        return arr[n] % 1234567;
+    }
+}
