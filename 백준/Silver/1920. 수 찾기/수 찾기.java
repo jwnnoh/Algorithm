1d24fc3f8d3ea6789cafee181d0791a887da98de
Jeongwan Noh <jeongwan.noh@gmail.com>
2024-04-02 15:17:28 +0900

    [Silver IV] Title: 수 찾기, Time: 560 ms, Memory: 48812 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int value = Integer.parseInt(st.nextToken());
+
+            if (set.contains(value)) {
+                sb.append(1).append("\n");
+                continue;
+            }
+            sb.append(0).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
