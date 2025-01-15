ae9b532b83ff7ade9eccb8c72f844daa359d45b0
Jeongwan Noh <jwnnoh@gmail.com>
2025-01-15 18:28:43 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+
+        for (String i : s1) {
+            for (String j : s2) {
+                if (i.equals(j)) {
+                    answer += 1;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
